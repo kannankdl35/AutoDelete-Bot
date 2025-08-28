087df6da38a76f1f37661ec26124a38a4cd7a29d
kannankdl35 <145252426+kannankdl35@users.noreply.github.com>
2025-08-28 15:38:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ CHAT_IDS = [
 # Duration (in seconds) for each chat_id before messages are auto-deleted
 ID_DUR = {
     -1001727392022: 300,   # in seconds
-    -1001913860698: 300   # in seconds
+    -1001913860698: 600   # in seconds
 }
 
 # Telegram API credentials (replace with your own in private .env or config file)
@@ -16,3 +16,4 @@ API_HASH = "77275414baa7c8a24fb8fd26d7db5adc"
 BOT_TOKEN = "8189496693:AAF-mPjYjs0R9SYAr-uA1vDBhu3mOBEr5JI"
 
 SESSION = "BQFXbW8Aq-wuen3EaZs94u8d33cDWwU3ERrGSo0P4kfyAaAfDl6b8wiN9acwrz0jSw4roqB1oSjwz9_kt3wWWfWwwkAZM761YsFCwo2qVZSBlQD2KIwoWCxtJaOlwIiVdktrSLBcrqJlDdt5PQOm1K3UpWtMDogrmq28vMUCUc4IyvELNnDhtJALnsF839XfZrDtegTIHzloC3brH_2PzLhaJ8EGRFAnDyrvXzooBW8q_w5dyTHfwqk_Ryi8j-EaT7fYVSvfPsOmtJXYycWBQMWULgq1DTTZW9OZPsvuroq84GVCvfLAcOKWSh5V9_vMkt01lkZzAbNleNQFJ0HqS0U50aV2zwAAAAB3XvERAA"
+
