c72aab7319ee9b4039d87e5b0929c1ce68e4caf6
kannankdl35 <145252426+kannankdl35@users.noreply.github.com>
2025-08-28 14:59:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # List of channel/group IDs to monitor
 CHAT_IDS = [
-    -1001111111111,
-    -1002222222222
+    -1001727392022,
+    -1001913860698
 ]
 
 # Duration (in seconds) for each chat_id before messages are auto-deleted
@@ -16,6 +16,7 @@ API_HASH = "77275414baa7c8a24fb8fd26d7db5adc"
 BOT_TOKEN = "8189496693:AAF-mPjYjs0R9SYAr-uA1vDBhu3mOBEr5JI"
 
 SESSION = "BQFXbW8AY-IVfRsbzzT31AGTSarD7iQs2MnnuQJsUBeA0Kdnseoc-F_TTBTbB8eGlCeJ4Bd9yWpK6IgPIR91GSW1ePto7w-NF1WKD4u5ycER2I_w2Zsq2tsuQw6YLvjhKQK68Su4S7Acm073LfZfczMNkMhbc1DwoyUPjZc6vchfTNgG20w6GPizLD7BM5ZXTarnOIthAdtl8Kh2i6UuDhvksWU7vLmQV8Ic7yefAVxrFOHf2RtqK1d1dPWDoEGKEzi8Vd2OrlvGN2ukgclHkpmZO9FLYweX2EyOrInJ1bjQ4pfFADo53eeard41fnIgfe5QCmPTbzmCgSeBtEjy2N61wFSGuAAAAAB3XvERAA"
+
 
 
 
